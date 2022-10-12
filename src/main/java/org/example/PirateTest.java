e0bc211ed9502cccc3dcd9c15730615785f08410
BuildTools <unconfigured@null.spigotmc.org>
2022-10-12 12:57:16 -0400

    U-TEST COMMIT 1: Added rollDice test to test Dice rolling method

---
+++ src/main/java/org/example/PirateTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PirateTest {
+
+    @Test
+    @DisplayName("Dice Roll")
+    void rollDice(){
+        Player p = new Player("Test");
+        p.roll("12345678");
+
+
+        for (Player.Dice die : p.dice){
+            for (Faces f : Faces.values()){
+                if (f.equals(die.face)){
+                    assertEquals(f, die.face);
+                }
+            }
+
+        }
+    }
+}
