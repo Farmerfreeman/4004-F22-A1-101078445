4804dc38514002cb546a32f4875ca74d9a929f96
Aaron Fisher <aaronfisher@cmail.carleton.ca>
2022-10-12 11:37:47 -0400

    First Commit - Boilerplate & Networking Code

---
+++ src/main/java/org/example/Config.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Config {
+
+    public static final int GAME_SERVER_PORT_NUMBER = 3010;
+
+    public static final int NUM_OF_PLAYERS = 3;
+
+}
---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,162 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Player object that connects to GameServer
+ *
+ * @author Sebastian Gadzinski
+ * @author Tarnish
+ */
+
+
+public class Player implements Serializable{
+
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public String name;
+
+    int playerId = 0;
+    Client clientConnection;
+    Player[] players = new Player[3];
+
+    public Player getPlayer() {
+        return this;
+    }
+
+    /*
+     * ----------Networking Code------------
+     */
+
+
+    //region Client Methods
+    public void sendStringToServer(String str) {
+        clientConnection.sendString(str);
+    }
+
+    public void connectToClient() {
+        clientConnection = new Client();
+    }
+
+    public void connectToClient(int port) {
+        clientConnection = new Client(port);
+    }
+
+    public void killClient() {
+        clientConnection.terminate();
+    }
+
+
+    public void initializePlayers() {
+        for (int i = 0; i < 3; i++) {
+            players[i] = new Player(" ");
+        }
+    }
+    //endregion
+
+
+
+    public void startGame() {
+
+    }
+
+
+
+    private class Client {
+        Socket socket;
+        private ObjectInputStream dIn;
+        private ObjectOutputStream dOut;
+
+        public Client() {
+            try {
+                socket = new Socket("localhost", Config.GAME_SERVER_PORT_NUMBER);
+                dOut = new ObjectOutputStream(socket.getOutputStream());
+                dIn = new ObjectInputStream(socket.getInputStream());
+
+                playerId = dIn.readInt();
+
+                System.out.println("Connected as " + playerId);
+                sendPlayer();
+
+            } catch (IOException ex) {
+                System.out.println("Client failed to open");
+            }
+        }
+
+        public Client(int portId) {
+            try {
+                socket = new Socket("localhost", portId);
+                dOut = new ObjectOutputStream(socket.getOutputStream());
+                dIn = new ObjectInputStream(socket.getInputStream());
+
+                playerId = dIn.readInt();
+
+                System.out.println("Connected as " + playerId);
+                sendPlayer();
+
+            } catch (IOException ex) {
+                System.out.println("Client failed to open");
+            }
+        }
+
+
+        public void sendPlayer() {
+            try {
+                dOut.writeObject(getPlayer());
+                dOut.flush();
+            } catch (IOException ex) {
+                System.out.println("Player not sent");
+                ex.printStackTrace();
+            }
+        }
+
+        /*
+         * function to send strings
+         */
+        public void sendString(String str) {
+            try {
+                dOut.writeUTF(str);
+                dOut.flush();
+            } catch (IOException ex) {
+                System.out.println("String not sent");
+                ex.printStackTrace();
+            }
+        }
+
+
+        void terminate() {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("Score sheet not received");
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /*
+     * ---------Constructor and Main class-----------
+     */
+
+    /*
+     * constructor takes the name of the player and sets the score to 0
+     */
+    public Player(String n) {
+        name = n;
+    }
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("What is your name ? ");
+        String name = myObj.next();
+        Player p = new Player(name);
+        p.initializePlayers();
+        p.connectToClient();
+        p.startGame();
+        myObj.close();
+    }
+}
