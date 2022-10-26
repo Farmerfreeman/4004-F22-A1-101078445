29334f63feac4c5c3cf397a69fdc06591fccda3c
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 15:22:55 -0400

    A-TEST COMMIT ROW 67

--- src/main/java/org/example/PirateTest.java
+++ src/main/java/org/example/PirateTest.java
@@ -524,6 +524,29 @@ public class PirateTest {
         assertEquals(9000, score);
     }
 
+    @Test
+    @DisplayName("A-TEST ROW 67")
+    void row67() {
+        Player p = new Player("test");
+        p.draw();
+        String[] held = {"1","2", "3", "4", "5", "6"};
+
+        p.card = Cards.GOLD;
+        p.game.rollDice(p.dice);
+        p.dice[0].face = Faces.MONKEY;
+        p.dice[1].face = Faces.MONKEY;
+        p.dice[2].face = Faces.MONKEY;
+        p.dice[3].face = Faces.MONKEY;
+        p.dice[4].face = Faces.MONKEY;
+        p.dice[5].face = Faces.MONKEY;
+        p.dice[6].face = Faces.SWORD;
+        p.dice[7].face = Faces.SWORD;
+        p.game.reRollNotHeld(p.dice, held);
+        p.dice[6].face = Faces.MONKEY;
+        p.dice[7].face = Faces.MONKEY;
+        int score = p.scoreDice();
+        assertEquals(4600, score);
+    }
 
 
 
