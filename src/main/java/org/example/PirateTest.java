032674140e1a99c8a70def96ebf31faee786dcb5
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 17:40:26 -0400

    A-TEST COMMIT ROW 78

--- src/main/java/org/example/PirateTest.java
+++ src/main/java/org/example/PirateTest.java
@@ -10,6 +10,7 @@ import java.util.Dictionary;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 public class PirateTest {
 
@@ -699,9 +700,38 @@ public class PirateTest {
         p.dice[6].face = Faces.MONKEY;
         p.dice[7].face = Faces.MONKEY;
         p.dice[5] = p.game.useSorcress(p.dice[5]);
+        p.dice[5].face = Faces.MONKEY;
         int score = p.scoreDice();
         assertEquals(500, score);
     }
+
+    @Test
+    @DisplayName("A-TEST ROW 78")
+    void row78() {
+        Player p = new Player("test");
+        p.draw();
+        String[] held = {"1","2", "3", "4", "5", "6"};
+
+        p.card = Cards.SORCERESS;
+        p.game.rollDice(p.dice);
+        p.dice[0].face = Faces.SKULL;
+        p.dice[1].face = Faces.SKULL;
+        p.dice[2].face = Faces.SKULL;
+        p.dice[3].face = Faces.PARROT;
+        p.dice[4].face = Faces.PARROT;
+        p.dice[5].face = Faces.PARROT;
+        p.dice[6].face = Faces.SWORD;
+        p.dice[7].face = Faces.SWORD;
+        p.dice[0] = p.game.useSorcress(p.dice[0]);
+        p.dice[0].face = Faces.PARROT;
+        p.game.reRollNotHeld(p.dice, held);
+        p.dice[6].face = Faces.PARROT;
+        p.dice[7].face = Faces.PARROT;
+
+        int score = p.scoreDice();
+        assertEquals(1000, score);
+    }
+
 
 
 
