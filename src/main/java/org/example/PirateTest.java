0353b5ee5eba230b88dabe30b8e83c91b22eb321
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 17:31:34 -0400

    A-TEST COMMIT ROW 110

--- src/main/java/org/example/PirateTest.java
+++ src/main/java/org/example/PirateTest.java
@@ -1115,5 +1115,30 @@ public class PirateTest {
         assertEquals(0, p.score);
     }
 
+    @Test
+    @DisplayName("A-TEST ROW 110")
+    /*This test involves using the playTurn(Dice[][]) function to test skull island functionality
+    This means that the interface will be present but the test will feed input to the scanner on its own, no input user is
+    necessary. This is because the Skull Island code is not a function in itself, but essentially a block in the playTurn
+    function.
+     This involves generating all the dice sets for each roll individually beforehand, and then feeding them into the function.*/
+    void row110(){
+        byte[] in = "Y\nN".getBytes();
+        ByteArrayInputStream input = new ByteArrayInputStream(in);
 
+
+        Player p = new Player("Test");
+        Player.Dice[][] diceset = new Player.Dice[2][8];
+        diceset[0] = new Player.Dice[] {new Player.Dice(Faces.SKULL), new Player.Dice(Faces.SKULL),
+                new Player.Dice(Faces.SKULL), new Player.Dice(Faces.SKULL), new Player.Dice(Faces.SKULL),
+                new Player.Dice(Faces.MONKEY), new Player.Dice(Faces.MONKEY), new Player.Dice(Faces.MONKEY)};
+        diceset[1] = new Player.Dice[] {new Player.Dice(Faces.SKULL), new Player.Dice(Faces.SKULL),
+                new Player.Dice(Faces.SKULL), new Player.Dice(Faces.SKULL), new Player.Dice(Faces.SKULL),
+                new Player.Dice(Faces.SKULL), new Player.Dice(Faces.SKULL), new Player.Dice(Faces.COIN)};
+        System.setIn(input);
+        int deduction = p.playTurn(diceset, Cards.CAPTAIN);
+
+        assertEquals(-1400, deduction);
+        assertEquals(0, p.score);
+    }
 }
