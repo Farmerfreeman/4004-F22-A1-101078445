4a2f91af53f071eb9761f992d56ab4588597cede
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 21:07:44 -0400

    A-TEST COMMIT ROW 92

--- src/main/java/org/example/PirateTest.java
+++ src/main/java/org/example/PirateTest.java
@@ -866,6 +866,44 @@ public class PirateTest {
         assertEquals(1100, score);
     }
 
+    @Test
+    @DisplayName("A-TEST ROW 92")
+    void row92() {
+        Player p = new Player("test");
+        p.draw();
+        String[] chest = {"6", "7", "8"};
+        String[] held = {"1","2", "6","7", "8"};
+
+
+        p.card = Cards.TREASURE_CHEST;
+        p.game.rollDice(p.dice);
+        p.dice[0].face = Faces.SKULL;
+        p.dice[1].face = Faces.SKULL;
+        p.dice[2].face = Faces.PARROT;
+        p.dice[3].face = Faces.PARROT;
+        p.dice[4].face = Faces.PARROT;
+        p.dice[5].face = Faces.COIN;
+        p.dice[6].face = Faces.COIN;
+        p.dice[7].face = Faces.COIN;
+        p.placeInChest(chest);
+        p.game.reRollNotHeld(p.dice, held);
+        p.dice[2].face = Faces.DIAMOND;
+        p.dice[3].face = Faces.DIAMOND;
+        p.dice[4].face = Faces.COIN;
+        chest = new String[] {"5"};
+        p.placeInChest(chest);
+        held = new String[]{"1", "2", "5", "6", "7", "8"};
+
+        p.game.reRollNotHeld(p.dice, held);
+        p.dice[2].face = Faces.SKULL;
+        p.dice[3].face = Faces.DIAMOND;
+
+
+        int score = p.scoreDice(p.chest);
+        assertEquals(600, score);
+    }
+
+
 
 
 }
