231ad7448b4c38a0ad962bd621f7fea192b644e7
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 19:24:25 -0400

    A-TEST COMMIT ROW 120:

--- src/main/java/org/example/PirateTest.java
+++ src/main/java/org/example/PirateTest.java
@@ -1291,4 +1291,24 @@ public class PirateTest {
         assertEquals(500, p.score);
     }
 
+    @Test
+    @DisplayName("A-TEST ROW 120")
+    void row120(){
+        Player p = new Player("Test");
+        p.game.rollDice(p.dice);
+        p.draw();
+        p.card = Cards.SEA_BATTLE_3;
+        p.dice[0].face = Faces.MONKEY;
+        p.dice[1].face = Faces.MONKEY;
+        p.dice[2].face = Faces.MONKEY;
+        p.dice[3].face = Faces.SWORD;
+        p.dice[4].face = Faces.SWORD;
+        p.dice[5].face = Faces.SWORD;
+        p.dice[6].face = Faces.SWORD;
+        p.dice[7].face = Faces.SKULL;
+
+        p.score = p.game.seaBattle(p.dice, p.card);
+        assertEquals(800, p.score);
+    }
+
 }
